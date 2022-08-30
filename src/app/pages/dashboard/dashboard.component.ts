aa77c841bfa67fde33a5ee8c94fd16f798766dcd
FilipCichowski <58987436+FilipCichowski@users.noreply.github.com>
2022-08-30 11:29:26 +0200

    Added displaying dialog on pasting URL

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { UserDataService } from '../../services/user-data.service';
-import { catchError, Observable, of, Subscription } from "rxjs";
+import { catchError, combineLatestWith, Observable, of, Subscription } from "rxjs";
 import { User } from '../../interfaces/user-interface';
 import { MatTableDataSource } from '@angular/material/table';
 import { DisplayUserInfoDialogComponent } from '../../components/display-user-info-dialog/display-user-info-dialog.component';
 import { DisplayUserInfoService } from '../../services/display-user-info.service';
-import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
@@ -13,7 +13,7 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-  result$!: Observable<User[]>;
+  result$!: Observable<[User[], Params] | never[]>;
   resultSubscription!: Subscription
   data!: User[];
   displayedColumns: string[] = ['name', 'gender', 'age'];
@@ -21,9 +21,9 @@ export class DashboardComponent implements OnInit, OnDestroy {
   dataSource!: MatTableDataSource<User[]>;
 
   highlight(row: any, evt: any) {
-    //console.log(evt, row);
-    this.displayUserInfoService.openUserInfoDialog(row);
     this.displayUserInfoService.addUserIdToURL(row.id);
+    this.displayUserInfoService.openUserInfoDialog(row);
+
   }
 
   constructor(
@@ -35,18 +35,19 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.result$ = this.userService.getUsers().pipe(
+      combineLatestWith(this.activatedRoute.queryParams),
       catchError((err) => {
         console.error(err);
         return of([]);
       })
     );
-    this.resultSubscription = this.result$.subscribe((res) => {
+    this.resultSubscription = this.result$.subscribe(([res, par]) => {
       this.data = res;
       this.dataSource = new MatTableDataSource<any>(this.data);
+      if (par["id"] !== undefined) {
+        this.displayUserInfoService.openUserInfoDialog(this.data.find(e => e.id === par["id"]))
+      }
     });
-
-    // this.activatedRoute.params.subscribe((params) => console.log(params));
-    // this.router.events.subscribe(e => console.table(e));
   }
 
   ngOnDestroy() {
--- src/app/services/display-user-info.service.ts
+++ src/app/services/display-user-info.service.ts
@@ -11,16 +11,28 @@ import { DisplayUserInfoDialogComponent } from '../components/display-user-info-
   providedIn: 'root',
 })
 export class DisplayUserInfoService {
+  // openUserInfoDialog(rowData: any) {
+  //   let dialogRef = this.dialog.open(DisplayUserInfoDialogComponent, {
+  //     height: '300px',
+  //     width: '300px',
+  //     data: { rowData: rowData },
+  //   });
+  //   this.addUserIdToURL(rowData.id);
+  //   dialogRef.afterClosed().subscribe((result) => {
+  //     this.addUserIdToURL(null);
+  //   });
+  // }
+
   openUserInfoDialog(rowData: any) {
     let dialogRef = this.dialog.open(DisplayUserInfoDialogComponent, {
       height: '300px',
       width: '300px',
       data: { rowData: rowData },
     });
-    this.addUserIdToURL(rowData.id);
-    dialogRef.afterClosed().subscribe((result) => {
-      this.addUserIdToURL(null);
-    });
+    // this.addUserIdToURL(rowData.id);
+    // dialogRef.afterClosed().subscribe((result) => {
+    //   this.addUserIdToURL(null);
+    // });
   }
 
   addUserIdToURL(userId: string | null) {
