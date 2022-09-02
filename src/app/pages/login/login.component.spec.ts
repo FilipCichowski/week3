09a8e1fdec488703d0b98e852190202db50316c0
FilipCichowski <58987436+FilipCichowski@users.noreply.github.com>
2022-09-02 16:22:14 +0200

    Working form test

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -7,12 +7,14 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
 import { By } from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AuthService} from "src/app/services/auth.service";
+import {AuthService} from "../../services/auth.service";
 import { LoginComponent } from "./login.component";
+import SpyInstance = jest.SpyInstance;
 
 let component: LoginComponent;
 let fixture: ComponentFixture<LoginComponent>;
 let debugElement: DebugElement;
+let submitSpy: SpyInstance;
 
 const numberOfInputs: number = 2;
 
@@ -25,6 +27,61 @@ const FakeAuth: Pick<AuthService, keyof AuthService> = {
   login(): void {}
 };
 
+function findEl<T>(
+  fixture: ComponentFixture<T>,
+  testId: string
+): DebugElement {
+  return fixture.debugElement.query(
+    By.css(`[test-id="${testId}"]`)
+  );
+}
+
+export function click<T>(
+  fixture: ComponentFixture<T>,
+  testId: string
+): void {
+  const element = findEl(fixture, testId);
+  const event = makeClickEvent(element.nativeElement);
+  element.triggerEventHandler('click', event);
+}
+
+export function makeClickEvent(
+  target: EventTarget
+): Partial<MouseEvent> {
+  return {
+    preventDefault(): void {},
+    stopPropagation(): void {},
+    stopImmediatePropagation(): void {},
+    type: 'click',
+    target,
+    currentTarget: target,
+    bubbles: true,
+    cancelable: true,
+    button: 0
+  };
+}
+
+export function setFieldValue<T>(
+  fixture: ComponentFixture<T>,
+  testId: string,
+  value: string,
+): void {
+    findEl(fixture, testId).nativeElement.value = value;
+}
+
+const fillForm = () => {
+  setFieldValue(fixture, "email", "abc@example.com");
+  setFieldValue(fixture, "password", "Abc12345!");
+}
+
+beforeAll(() => {
+  submitSpy = jest.spyOn(LoginComponent.prototype, "onLogin").mockImplementation(() => {});
+})
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
 beforeEach(async () => {
   await TestBed.configureTestingModule({
     declarations: [LoginComponent],
@@ -36,6 +93,9 @@ beforeEach(async () => {
       MatCardModule,
       ReactiveFormsModule
     ],
+    providers: [
+      {provide: AuthService, useValue: FakeAuth}
+    ]
   }).compileComponents();
 });
 
@@ -51,3 +111,23 @@ it("the number of elements rendered in UI is equal to the form controls defined 
   const inputElements = formElement.queryAll(By.css('input'))
   expect(inputElements.length).toBe(numberOfInputs);
 })
+
+it("initial values is form should be empty", () => {
+  const formElement = debugElement.query(By.css('[test-id="form"]'));
+  const inputElements = formElement.queryAll(By.css('input'))
+  const inputValues = inputElements.map(e => e.nativeElement.value);
+  expect(inputValues).toEqual(Array(numberOfInputs).fill(""));
+})
+
+it('submits the form successfully', () => {
+  fillForm();
+  findEl(fixture, 'form').triggerEventHandler('submit', {});
+  expect(component.onLogin).toBeCalled();
+});
+
+it('can submit form with button click', () => {
+  fillForm();
+  findEl(fixture, 'submit').triggerEventHandler('click', {});
+  expect(component.onLogin).toBeCalled();
+})
+
